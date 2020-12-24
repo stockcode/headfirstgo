d28620322d5a948e20aaf6c708c2259c525bc42f
gengke <geng_ke@sina.com>
2020-12-24 16:33:36 +0800

    Inital commit

---
+++ chapter1/hello.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "os"
+  "log"
+  "strconv"
+  "strings"
+)
+
+func main() {
+
+  fmt.Print("Enter a grade: ")
+  reader := bufio.NewReader(os.Stdin)
+  input, err := reader.ReadString('\n')
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  input = strings.TrimSpace(input)
+  grade, err := strconv.ParseFloat(input, 64)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  var status string
+
+  if int(grade) >= 60 {
+    status = "passing"
+  } else {
+    status = "failing"
+  }
+
+  fmt.Println("A grade of", grade, "is", status)
+
+
+
+
+
+
+}
