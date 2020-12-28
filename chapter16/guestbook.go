89f1de57b2f540119505e6085ad7524603d79cc9
gengke <geng_ke@sina.com>
2020-12-28 21:09:51 +0800

    finish chapter16

---
+++ chapter16/guestbook.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	signatures := getStrings("signatures.txt")
+
+	html, err := template.ParseFiles("view.html")
+	check(err)
+
+	guestbook := Guestbook{
+		SignatureCount: len(signatures),
+		Signatures:     signatures,
+	}
+
+	err = html.Execute(writer, guestbook)
+	check(err)
+}
+
+func newHandler(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("new.html")
+	check(err)
+
+	err = html.Execute(writer, nil)
+	check(err)
+}
+
+func createHandler(writer http.ResponseWriter, request *http.Request) {
+	signature := request.FormValue("signature")
+
+	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+	file, err := os.OpenFile("signatures.txt", options, os.FileMode(0600))
+	check(err)
+
+	_, err = fmt.Fprintln(file, signature)
+	check(err)
+
+	err = file.Close()
+	check(err)
+
+	http.Redirect(writer, request, "/guestbook", http.StatusFound)
+}
+
+func executeTemplate(text string, data interface{}) {
+	tmpl, err := template.New("test").Parse(text)
+	check(err)
+	err = tmpl.Execute(os.Stdout, data)
+	check(err)
+}
+
+type Part struct {
+	Name  string
+	Count int
+}
+
+func getStrings(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
+}
+
+type Guestbook struct {
+	SignatureCount int
+	Signatures     []string
+}
+
+func main() {
+	http.HandleFunc("/guestbook", viewHandler)
+	http.HandleFunc("/guestbook/new", newHandler)
+	http.HandleFunc("/guestbook/create", createHandler)
+	err := http.ListenAndServe("localhost:8080", nil)
+	log.Fatal(err)
+
+	tmplateText := "Name: {{.Name}}\nCount: {{.Count}}\n"
+
+	executeTemplate(tmplateText, Part{
+		Name:  "Fuses",
+		Count: 5,
+	})
+
+	executeTemplate(tmplateText, Part{
+		Name:  "Cables",
+		Count: 2,
+	})
+}
